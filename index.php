b1bd4fc0b6043952d4b3ffe75777d718c5356c7e
Jackson Steel <jackson-steel@hotmail.com>
2016-07-02 23:55:10 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Home Page | Joshua Lloyd Programming Portfolio</title>
+		<!-- Include Styles -->
+		<link rel="stylesheet" href="css/reset.css" />
+		<link rel="stylesheet" href="css/main.css" />
+	</head>
+
+	<body>
+		<div id="container">
+			<header> <!-- START Header Section -->
+				<h1>Joshua Lloyd Programming Portfolio</h1>
+				<h1>Where the advert should be</h1>
+				
+				<nav> <!-- START Navbar -->
+					<ul>
+						<li id="active"><a href="#">Projects</a></li>
+						<li><a href="tutorials.php">Tutorials</a></li>
+						<li><a href="resume-cv.php">Resume (CV)</a></li>
+						<li><a href="contact.php">Contact Page</a><li>
+					</ul>
+				</nav> <!-- END Navbar -->
+				
+			</header>  <!-- END Header Section -->
+			
+			<article> <!-- START Content Section -->
+				<section> <!-- START Section -->
+					<p>Below are all the projects I've designed and developed
+					myself using a variety of programming languages and tools.</p>
+					
+					<!-- TASK: INSERT PHP SCRIPT THAT INSERTS ALL
+					PROJECT PAGES FROM THE DATABASE LOCATED
+					ON OUR WEB SERVER -->
+					
+				</section> <!-- END Section -->
+			</article> <!-- END Content Section -->
+		</div>
+	
+		<!-- Include JavaScripts -->
+		<script src="js/main.js"></script>
+	</body>
+</html>
---
+++ tutorials.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Tutorials | Joshua Lloyd Programming Portfolio</title>
+		<!-- Include Styles -->
+		<link rel="stylesheet" href="css/reset.css" />
+		<link rel="stylesheet" href="css/main.css" />
+	</head>
+
+	<body>
+		<div id="container">
+			<header> <!-- START Header Section -->
+				<h1>Joshua Lloyd Programming Portfolio</h1>
+				<h1>Where the advert should be</h1>
+				
+				<nav> <!-- START Navbar -->
+					<ul>
+						<li><a href="index.php">Projects</a></li>
+						<li id="active"><a href="#">Tutorials</a></li>
+						<li><a href="resume-cv.php">Resume (CV)</a></li>
+						<li><a href="contact.php">Contact Page</a><li>
+					</ul>
+				</nav> <!-- END Navbar -->
+			</header>  <!-- END Header Section -->
+			
+			<article> <!-- START Content Section -->
+				<section> <!-- START Section -->
+				</section> <!-- END Section -->
+			</article> <!-- END Content Section -->
+		</div>
+	
+		<!-- Include JavaScripts -->
+		<script src="js/main.js"></script>
+	</body>
+</html>
